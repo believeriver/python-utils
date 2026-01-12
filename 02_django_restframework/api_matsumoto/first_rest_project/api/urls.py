390dce872cf805925068e34223236ba3357771f0
believeriver <nobuyuki.galois@gmail.com>
2026-01-12 21:56:36 +0900

    20260112 03 study django_restframework.

--- 02_django_restframework/api_matsumoto/first_rest_project/api/urls.py
+++ 02_django_restframework/api_matsumoto/first_rest_project/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('json-example/', views.json_example, name='json_example'),
+    path('country-datetime/', views.country_datetime, name='country_datetime'),
 ]
--- 02_django_restframework/api_matsumoto/first_rest_project/api/views.py
+++ 02_django_restframework/api_matsumoto/first_rest_project/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+from datetime import datetime
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 def index(request):
     return HttpResponse("<h1>Welcome to the First REST Project!</h1>")
@@ -12,4 +14,15 @@ def json_example(request):
         "status": "success"
     }
     return JsonResponse(data)
+
+
+@api_view(['GET'])
+def country_datetime(request):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data = {
+        "country": "Japan",
+        "current_datetime": current_time
+    }
+    return Response(data)
+
 
