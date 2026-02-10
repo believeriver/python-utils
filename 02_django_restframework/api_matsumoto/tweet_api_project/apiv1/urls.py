349faea9013d322c5d5c8d49e37d987ef9b185a4
believeriver <nobuyuki.galois@gmail.com>
2026-02-11 08:34:59 +0900

    20260211 01 tweets create user register

--- 02_django_restframework/api_matsumoto/tweet_api_project/apiv1/urls.py
+++ 02_django_restframework/api_matsumoto/tweet_api_project/apiv1/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tweets/', views.TweetListView.as_view(), name='tweet-list'),
+    path('tweets/', views.TweetListView.as_view(), name='tweets-list'),
+    path('user/register/', views.UserRegisterView.as_view(), name='user-register'),
 ]
--- 02_django_restframework/api_matsumoto/tweet_api_project/apiv1/views.py
+++ 02_django_restframework/api_matsumoto/tweet_api_project/apiv1/views.py
@@ -1,5 +1,27 @@
+from rest_framework.permissions import AllowAny
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+
+from .serializers import UserRegisterSerializer
+
+class UserRegisterView(APIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response(
+                {
+                    "message": "User registered successfully.",
+                    "user": UserRegisterSerializer(user).data
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TweetListView(APIView):
