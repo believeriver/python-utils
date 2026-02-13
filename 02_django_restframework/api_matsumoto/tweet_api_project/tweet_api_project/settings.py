92b8881ffaa39751c5895268f4c46a01181d0979
believeriver <nobuyuki.galois@gmail.com>
2026-02-14 08:35:49 +0900

    20260214 02 add corsheader

--- 02_django_restframework/api_matsumoto/tweet_api_project/tweet_api_project/settings.py
+++ 02_django_restframework/api_matsumoto/tweet_api_project/tweet_api_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
     'rest_framework.authtoken',
     'tweet_api_project',
     'apiv1',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,3 +140,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+# CORS Configuration
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
