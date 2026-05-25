6343b7040303c3572bbf7ea1cd476cae737b589b
believeriver <nobuyuki.galois@gmail.com>
2026-05-26 04:59:29 +0900

    20260526 02

---
+++ 02_django_restframework/claude_teach/frontend/src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import marketReducer from "./features/market/marketSlice";
+
+
+export const store = configureStore({
+    reducer: {
+        market: marketReducer, //機能ごとにリデューサーを追加
+    },
+})
+
+
+// Typescript用の型定義
+// useSelector / useDispatchで型安全にアクセスできるようにする
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
