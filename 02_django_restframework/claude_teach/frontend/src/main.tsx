f1964b971b0c0891fdcbfce9f9dae1c76f6cb46d
believeriver <nobuyuki.galois@gmail.com>
2026-06-18 05:08:21 +0900

    20260618 01

--- 02_django_restframework/claude_teach/frontend/src/main.tsx
+++ 02_django_restframework/claude_teach/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store'
 import './index.css'
 import App from './App.tsx'
@@ -9,7 +10,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* Provider : 全コンポーネントから Store にアクセスできるようにする */}
     <Provider store={store}>
-      <App />
+      {/* BrowserRouter : URLベースのルーティングを有効にする */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
